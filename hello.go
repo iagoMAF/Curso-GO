218eb4fb340617d30a396b3e653e61152c19cf14
Iago Fernandes <iago.avila@ufvjm.edu.br>
2023-11-25 17:42:21 -0300

    Finalizando o modulo 5

--- hello.go
+++ hello.go
@@ -1,13 +1,19 @@
+// git push --set-upstream origin Main
 package main
 
 import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 )
+
+const monitoramentos = 3
+const delay = 5
 
 func main() {
 	exibeIntroducao()
+
 	for {
 		exibeMenu()
 		comando := leComando()
@@ -27,12 +33,6 @@ func main() {
 	}
 }
 
-// func devolveNomeEIdade() (string, int) {
-// 	nome := "Iago"
-// 	idade := 23
-// 	return nome, idade
-// }
-
 func exibeIntroducao() {
 	nome := "Iago"
 	versao := 0.1
@@ -50,20 +50,33 @@ func leComando() int {
 	var comandoLido int
 	fmt.Scan(&comandoLido)
 	fmt.Println("O comando escolhido foi", comandoLido)
+	fmt.Println("")
 	return comandoLido
 }
 
 func iniciarMonitoramento() {
 	fmt.Println("Monitorando..")
-	site := "https://www.alura.com.br"
-	// site := "https://random-status-code.herokuapp.com/"
+	sites := []string{"https://random-status-code.herokuapp.com/", "https://www.alura.com.br", "https://www.caelum.com.br"}
+
+	for i := 0; i < monitoramentos; i++ {
+		for i, site := range sites {
+			fmt.Println("Testando site", i, ":", site)
+			testaSite(site)
+			time.Sleep(delay * time.Second)
+		}
+		fmt.Println("")
+	}
+
+	fmt.Println("")
+}
+
+func testaSite(site string) {
 	resp, _ := http.Get(site)
-	fmt.Println(resp.StatusCode)
 
 	if resp.StatusCode == 200 {
 		fmt.Println("Site:", site, "foi carregado com sucesso!")
 	} else {
-		fmt.Println("Site", site, "esta com problema. Status code:", resp.StatusCode)
+		fmt.Println("Site", site, "esta com problema. Status code:",
+			resp.StatusCode)
 	}
-
 }
