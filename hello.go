868781fa79f73d9da42ffa622a2aa5e70f626762
Iago Fernandes <iago.avila@ufvjm.edu.br>
2023-11-28 14:56:27 -0300

    Finalizando o ultimo modulo

--- hello.go
+++ hello.go
@@ -4,8 +4,10 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -15,9 +17,6 @@ const delay = 2
 
 func main() {
 	exibeIntroducao()
-
-	leSitesDoArquivo()
-
 	for {
 		exibeMenu()
 		comando := leComando()
@@ -30,6 +29,7 @@ func main() {
 			iniciarMonitoramento()
 		case 2:
 			fmt.Println("Exibindo logs")
+			imprimeLogs()
 		default:
 			fmt.Println("Não existe este comando")
 			os.Exit(-1)
@@ -86,9 +86,11 @@ func testaSite(site string) {
 
 	if resp.StatusCode == 200 {
 		fmt.Println("Site:", site, "foi carregado com sucesso!")
+		registraLog(site, true)
 	} else {
 		fmt.Println("Site", site, "esta com problema. Status code:",
 			resp.StatusCode)
+		registraLog(site, false)
 	}
 }
 
@@ -114,4 +116,25 @@ func leSitesDoArquivo() []string {
 
 	arquivo.Close()
 	return sites
+}
+
+func registraLog(site string, status bool) {
+	arquivo, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05") + " - " + site + " - online:" + strconv.FormatBool(status) + "\n")
+	arquivo.Close()
+}
+
+func imprimeLogs() {
+	arquivo, err := ioutil.ReadFile("log.txt")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(arquivo))
 }
